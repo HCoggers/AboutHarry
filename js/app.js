26caefab4a5cb67a06b61e019852e4716e76d13f
Harry Cogswell <harry.cogswell@gmail.com>
2019-01-09 17:06:44 -0800

    adjusted point system and added 6th question

--- js/app.js
+++ js/app.js
@@ -5,65 +5,89 @@ var name = prompt('Is my first name really Harrison? Y/N');
 console.log('First name:', name);
 
 if(name.toLowerCase() === 'no' || name.toLowerCase() === 'n'){
-  var score = 20;
-  alert('Good job! Your score is now ' + score);
+  var score = 1;
+  alert('Good job! Your score is now ' + score + '/7');
 }else{
   score = 0;
-  alert('Not Quite. Your score is ' + score);
+  alert('Not Quite. Your score is ' + score + '/7');
 }
-console.log('score is:', score);
+console.log('score is:', score, 'out of 7');
 
 var dance = prompt('Is tap my favorite style of dance?');
 console.log('Enjoys Tap:', dance);
 
 if(dance.toLowerCase() === 'no' || dance.toLowerCase() === 'n'){
-  score = (score + 20);
-  alert('Well done! Your Score is now ' + score);
+  score++;
+  alert('Well done! Your Score is now ' + score + '/7');
 }else{
-  alert('Try again. Your score is ' + score);
+  alert('Try again. Your score is ' + score + '/7');
 }
-console.log('score is:', score);
+console.log('score is:', score, 'out of 7');
 
 var pets = prompt('Do I like cats better than dogs?');
 console.log('Prefers cats:', pets);
 
 if(pets.toLowerCase() === 'yes' || pets.toLowerCase() === 'y'){
-  score = (score + 20);
-  alert('Exactly Right! Your Score is now ' + score);
+  score++;
+  alert('Exactly Right! Your Score is now ' + score + '/7');
 }else{
-  alert('C\'mon, that was an easy one! Your score is ' + score);
+  alert('C\'mon, that was an easy one! Your score is ' + score + '/7');
 }
-console.log('score is:', score);
+console.log('score is:', score, 'out of 7');
 
 var gen = prompt('Am I technically a millenial? #Guessinggame #Nofilter');
 console.log('Is Millenial:', gen);
 
 if(gen.toLowerCase() === 'yes' || gen.toLowerCase() === 'y'){
-  score = (score + 20);
-  alert('You Rock! Your Score is now ' + score);
+  score++;
+  alert('You Rock! Your Score is now ' + score + '/7');
 }else{
-  alert('You\'ll get it next time... Your score is ' + score);
+  alert('You\'ll get it next time... Your score is ' + score + '/7');
 }
-console.log('score is:', score);
+console.log('score is:', score, 'out of 7');
 
-var success = prompt('This is a tricky one. Do you think you passed this test?');
-console.log('Passing Grade:', success);
+var caffeine = prompt('Okay, here\'s an easy one. Do I prefer coffee over tea?');
+console.log('Drink preference is:', caffeine);
 
-if((score > 60 && success.toLowerCase() === 'yes') || (score > 60 && success.toLowerCase() === 'y')){
-  score = (score + 20);
-  alert('CONGRATULATIONS! YOU PASSED WITH ' + score + ' POINTS!');
-  alert('Your prize? I think knowing a cool dude like me is reward enough.');
-}else if(score > 80){
-  alert('WRONG! Which in this case is a good thing. YOU PASSED WITH ' + score + ' POINTS!');
-  alert('Your prize? I think knowing a cool dude like me is reward enough.');
-}else if(success.toLowerCase() === 'yes' || success.toLowerCase() === 'y'){
-  alert('WRONG! You don\'t seem to know me at all! You only scored ' + score + ' points...');
-  alert('Sorry, pal, you failed. refresh the page if you want to try again!');
+if(caffeine.toLowerCase() === 'yes' || caffeine.toLowerCase() === 'y'){
+  score++;
+  alert('I love my bean juice! Your Score is now ' + score + '/7');
 }else{
-  score = (score + 20);
-  alert('Unfortunately, that\'s correct. You only scored ' + score + ' points...');
-  alert('Sorry, pal, you failed. refresh the page if you want to try again!');
+  alert('Tea\'s alright, but not my preference... Your score is ' + score + '/7');
 }
+console.log('score is:', score, 'out of 7');
+
+//QUESTION 6
+
+var cream = prompt('So, now you know I love my coffee, can you guess how many sugars I use? It\'s less than 10, and I\'ll even give you 4 chances!')
+
+while (cream !== 3){
+  while(!(cream < 10) || cream < 0){
+    cream = prompt('Please type an integer between 0 and 9.');
+  }
+  for(var i = 0; i < 3; i++){
+    if (cream < 3){
+      cream = prompt ('More than that.');
+    }
+    else if (cream > 3) {
+      cream = prompt ('Less than that.')
+    } else {
+      break;
+    }
+  }
+  break;
+} 
+console.log('Number of sugars:', cream);
+
+cream = parseInt(cream);
+
+if (cream === 3) {
+  score++;
+  alert('You figured it out! Your score is now ' + score + '/7');
+} else {
+  alert('you couldn\'t get it. your score is ' + score + '/7');
+}
+
 
 console.log('final score is:', score);
 
