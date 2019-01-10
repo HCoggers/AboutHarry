e7895d2d24b1e00c096a26c11e68d66f94619fd0
aditya parthasarathy <surayasun59@gmail.com>
2019-01-10 14:52:48 -0800

    added function

--- js/app.js
+++ js/app.js
@@ -2,20 +2,23 @@
 /* eslint-disable strict */
 
 // Next Step: Add loops to Y/N questions, to ensure a yes or no answer. also, organize with more white space and comments
-
+var score = 0;
+function question1() {
 alert('How well do you know me? answer these yes or no questions ');
 var name = prompt('Is my first name really Harrison? Y/N');
 console.log('First name:', name);
 
 if(name.toLowerCase() === 'no' || name.toLowerCase() === 'n'){
-  var score = 1;
+  score = 1;
   alert('Good job! Your score is now ' + score + '/7');
 }else{
   score = 0;
   alert('Not Quite. Your score is ' + score + '/7');
 }
 console.log('score is:', score, 'out of 7');
+}
 
+function question2() {
 var dance = prompt('Is tap my favorite style of dance?');
 console.log('Enjoys Tap:', dance);
 
@@ -26,7 +29,9 @@ if(dance.toLowerCase() === 'no' || dance.toLowerCase() === 'n'){
   alert('Try again. Your score is ' + score + '/7');
 }
 console.log('score is:', score, 'out of 7');
+}
 
+function question3() {
 var pets = prompt('Do I like cats better than dogs?');
 console.log('Prefers cats:', pets);
 
@@ -37,7 +42,9 @@ if(pets.toLowerCase() === 'yes' || pets.toLowerCase() === 'y'){
   alert('C\'mon, that was an easy one! Your score is ' + score + '/7');
 }
 console.log('score is:', score, 'out of 7');
+}
 
+function question4() {
 var gen = prompt('Am I technically a millenial? #Guessinggame #Nofilter');
 console.log('Is Millenial:', gen);
 
@@ -48,7 +55,9 @@ if(gen.toLowerCase() === 'yes' || gen.toLowerCase() === 'y'){
   alert('You\'ll get it next time... Your score is ' + score + '/7');
 }
 console.log('score is:', score, 'out of 7');
+}
 
+function question5() {
 var caffeine = prompt('Okay, here\'s an easy one. Do I prefer coffee over tea?');
 console.log('Drink preference is:', caffeine);
 
@@ -59,15 +68,18 @@ if(caffeine.toLowerCase() === 'yes' || caffeine.toLowerCase() === 'y'){
   alert('Tea\'s alright, but not my preference... Your score is ' + score + '/7');
 }
 console.log('score is:', score, 'out of 7');
+}
 
 //QUESTION 6
 
+function question6() {
 var sugar = prompt('So, now you know I love my coffee, can you guess how many sugars I use? It\'s less than 10, and I\'ll even give you 4 chances!')
 
 if (sugar !== 3){
   while(!(sugar < 10) || sugar < 0){
     sugar = prompt('Please type an integer between 0 and 9.');
   }
+  sugar = parseInt(sugar);
   for(var i = 0; i < 3; i++){
     if (sugar < 3){
       sugar = prompt ('More than that.');
@@ -83,17 +95,17 @@ if (sugar !== 3){
 
 console.log('Number of sugars:', sugar);
 
-sugar = parseInt(sugar);
-
 if (sugar === 3) {
   score++;
   alert('You figured it out! Your score is now ' + score + '/7');
 } else {
   alert('you couldn\'t get it. your score is ' + score + '/7');
 }
+}
 
 //QUESTION 7
 
+function question7() {
 var lizArray = ['chameleon', 'bearded dragon', 'gecko']
 var lizard = prompt ('Last question! My sister owns a menagerie of different reptiles. Can you guess a one kind of lizard that she has? you\'ll get SIX tries this time.');
 
@@ -121,6 +133,14 @@ if (lizard.toLowerCase() !== lizArray[0] && lizard.toLowerCase() !== lizArray[1]
   score++;
   alert('She\'s got one of those! Good job! Your score is now ' + score + '/7'); 
 }
+}
+question1();
+question2();
+question3();
+question4();
+question5();
+question6();
+question7();
 
 console.log('final score is:', score);
 
