f285048d44edb46cfc6f27c0ecd681d4dab7a49a
Harry Cogswell <harry.cogswell@gmail.com>
2019-01-09 18:14:31 -0800

    added two more questions to the guessing game, and planned some next steps.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 /* eslint-disable no-console */
 /* eslint-disable strict */
+
+// Next Step: Add loops to Y/N questions, to ensure a yes or no answer. also, organize with more white space and comments
+
 alert('How well do you know me? answer these yes or no questions ');
 var name = prompt('Is my first name really Harrison? Y/N');
 console.log('First name:', name);
@@ -59,35 +62,65 @@ console.log('score is:', score, 'out of 7');
 
 //QUESTION 6
 
-var cream = prompt('So, now you know I love my coffee, can you guess how many sugars I use? It\'s less than 10, and I\'ll even give you 4 chances!')
+var sugar = prompt('So, now you know I love my coffee, can you guess how many sugars I use? It\'s less than 10, and I\'ll even give you 4 chances!')
 
-while (cream !== 3){
-  while(!(cream < 10) || cream < 0){
-    cream = prompt('Please type an integer between 0 and 9.');
+if (sugar !== 3){
+  while(!(sugar < 10) || sugar < 0){
+    sugar = prompt('Please type an integer between 0 and 9.');
   }
   for(var i = 0; i < 3; i++){
-    if (cream < 3){
-      cream = prompt ('More than that.');
-    }
-    else if (cream > 3) {
-      cream = prompt ('Less than that.')
+    if (sugar < 3){
+      sugar = prompt ('More than that.');
+    } else if (sugar > 3) {
+      sugar = prompt ('Less than that.');
+    } else if (sugar !== 3){
+      sugar = prompt ('try again');
     } else {
       break;
     }
   }
-  break;
-} 
-console.log('Number of sugars:', cream);
+}
 
-cream = parseInt(cream);
+console.log('Number of sugars:', sugar);
 
-if (cream === 3) {
+sugar = parseInt(sugar);
+
+if (sugar === 3) {
   score++;
   alert('You figured it out! Your score is now ' + score + '/7');
 } else {
   alert('you couldn\'t get it. your score is ' + score + '/7');
 }
 
+//QUESTION 7
+
+var lizArray = ['chameleon', 'bearded dragon', 'gecko']
+var lizard = prompt ('Last question! My sister owns a menagerie of different reptiles. Can you guess a one kind of lizard that she has? you\'ll get SIX tries this time.');
+
+if (lizard.toLowerCase() !== lizArray[0] && lizard.toLowerCase() !== lizArray[1] && lizard.toLowerCase() !== lizArray[2]) {
+  for (var n = 0; n < 6; n++) {
+    switch (lizard.toLowerCase()) {
+    case lizArray[0]:
+      break;
+    case lizArray[1]:
+      break;
+    case lizArray[2]:
+      break;
+    default:
+      lizard = prompt ('I\'ll let you try again. what kind of lizard?');
+      break;
+    }
+  }
+}
+
+console.log('lizard guess:', lizard);
+
+if (lizard.toLowerCase() !== lizArray[0] && lizard.toLowerCase() !== lizArray[1] && lizard.toLowerCase() !== lizArray[2]) {
+  alert('She doesn\'t have any of those. your score is ' + score + '/7');
+} else {
+  score++;
+  alert('She\'s got one of those! Good job! Your score is now ' + score + '/7'); 
+}
 
 console.log('final score is:', score);
 
