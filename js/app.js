bf61a3de4bc2aa779eb66d822ae68c3b44c4c0db
HCoggers <harry.cogswell@gmail.com>
2019-01-11 10:19:30 -0800

    use strict in js, and implemented new css selectors

--- js/app.js
+++ js/app.js
@@ -1,16 +1,17 @@
 /* eslint-disable no-console */
+'use strict';
 
 var score = 0;
 
 
 //Yes or No Questions
-function yesNoQuestions (){
+function yesNoQuestions() {
   var ynquestions = ['Is my first name really Harrison? Y/N', 'Is tap my favorite style of dance? Y/N', 'Do I like cats better than dogs? Y/N', 'Am I technically a millenial?  Y/N', 'Okay, here\'s an easy one. Do I prefer coffee over tea?'];
   var ynanswers = ['n', 'n', 'y', 'y', 'y'];
   var ynresponses = ['n', 'y'];
-  for (i=0; i<5; i++){
+  for (var i = 0; i < 5; i++) {
     var uresponse = prompt(ynquestions[i]);
-    while (uresponse !== ynresponses [0] && uresponse !== ynresponses [1]) {
+    while (uresponse !== ynresponses[0] && uresponse !== ynresponses[1]) {
       switch (uresponse.toLowerCase()) {
         case 'yes':
           uresponse = ynresponses[1];
@@ -23,13 +24,13 @@ function yesNoQuestions (){
           break;
       }
     }
-    if (uresponse.toLowerCase() === ynanswers[i]){
+    if (uresponse.toLowerCase() === ynanswers[i]) {
       score++;
-      alert ('Good job! Your score is now ' + score + '/7');
-    } else if (uresponse.toLowerCase() !== ynanswers[i]){
-      alert ('Not Quite. Your score is ' + score + '/7');
+      alert('Good job! Your score is now ' + score + '/7');
+    } else if (uresponse.toLowerCase() !== ynanswers[i]) {
+      alert('Not Quite. Your score is ' + score + '/7');
     }
-    console.log('answer', (i + 1), uresponse);
+    console.log('Answer', (i + 1), '=', uresponse);
   }
 }
 
@@ -38,26 +39,26 @@ function yesNoQuestions (){
 function question6() {
   var sugar = prompt('So, now you know I love my coffee, can you guess how many sugars I use? It\'s less than 10, and I\'ll even give you 4 chances!')
 
-  if (sugar !== 3){
-    while(!(sugar < 10) || sugar < 0){
+  if (sugar !== 3) {
+    while (!(sugar < 10) || sugar < 0) {
       sugar = prompt('Please type an integer between 0 and 9.');
     }
-    for(var i = 0; i < 3; i++){
+    for (var i = 0; i < 3; i++) {
       sugar = parseInt(sugar);
-      if (sugar < 3){
-        sugar = prompt ('More than that.');
+      if (sugar < 3) {
+        sugar = prompt('More than that.');
       } else if (sugar > 3) {
-        sugar = prompt ('Less than that.');
-      } else if (sugar === 3){
+        sugar = prompt('Less than that.');
+      } else if (sugar === 3) {
         break;
       } else {
-        sugar = prompt ('try again');
+        sugar = prompt('try again');
       }
       sugar = parseInt(sugar);
     }
   }
 
-  console.log('Number of sugars:', sugar);
+  console.log('Number of sugars =', sugar);
 
   if (sugar === 3) {
     score++;
@@ -71,31 +72,31 @@ function question6() {
 //QUESTION 7
 function question7() {
   var lizArray = ['chameleon', 'bearded dragon', 'gecko']
-  var lizard = prompt ('Last question! My sister owns a menagerie of different reptiles. Can you guess a one kind of lizard that she has? you\'ll get SIX tries this time.');
+  var lizard = prompt('Last question! My sister owns a menagerie of different reptiles. Can you guess a one kind of lizard that she has? you\'ll get SIX tries this time.');
 
   if (lizard.toLowerCase() !== lizArray[0] && lizard.toLowerCase() !== lizArray[1] && lizard.toLowerCase() !== lizArray[2]) {
-    for (var n = 0; n < 6; n++) {
+    for (var i = 0; i < 6; i++) {
       switch (lizard.toLowerCase()) {
-      case lizArray[0]:
-        break;
-      case lizArray[1]:
-        break;
-      case lizArray[2]:
-        break;
-      default:
-        lizard = prompt ('I\'ll let you try again. what kind of lizard?');
-        break;
+        case lizArray[0]:
+          break;
+        case lizArray[1]:
+          break;
+        case lizArray[2]:
+          break;
+        default:
+          lizard = prompt('I\'ll let you try again. what kind of lizard?');
+          break;
       }
     }
   }
 
-  console.log('lizard guess:', lizard);
+  console.log('Type of lizard =', lizard);
 
   if (lizard.toLowerCase() !== lizArray[0] && lizard.toLowerCase() !== lizArray[1] && lizard.toLowerCase() !== lizArray[2]) {
     alert('She doesn\'t have any of those. your score is ' + score + '/7');
   } else {
     score++;
-    alert('She\'s got one of those! Good job! Your score is now ' + score + '/7'); 
+    alert('She\'s got one of those! Good job! Your score is now ' + score + '/7');
   }
 }
 
@@ -105,7 +106,7 @@ yesNoQuestions();
 question6();
 question7();
 
-console.log('final score is:', score);
+console.log('Final score =', score);
 
 //DISPLAY SCORE ON WEBPAGE
-document.getElementById('scorestatement').innerHTML = score;
+document.getElementById('scorestatement').innerHTML = score + '/7';
