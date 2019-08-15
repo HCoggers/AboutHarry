8714b3435baf07acce55f283ac3c69a61eb01fac
Harrison Cogswell <harry.cogswell@gmail.com>
2019-08-14 18:06:41 -0700

    created yes or no feedback loop for one question, and translated it into the second question

--- js/app.js
+++ js/app.js
@@ -12,10 +12,42 @@
 
 // QUESTION 1
 
+var answerOne = prompt('Is my favorite color blue? yes or no.');
+
+while(answerOne.toLowerCase() !== 'y' && answerOne.toLowerCase() !== 'n' && answerOne.toLowerCase() !== 'yes' && answerOne.toLowerCase() !== 'no') {
+  answerOne = prompt('please answer yes or no.');
+}
+
+console.log('answerOne: ', answerOne);
+
+if (answerOne.toLowerCase() === 'n' || answerOne.toLowerCase() === 'no'){
+  alert('Correct! blue is not my favorite color. I\'m not sure what actually is though... Maybe hot pink.');
+} else {
+  alert('Incorrect! blue is not my favorite color. Shows how much you know...');
+}
+
 // QUESTION 2
+
+var answerTwo = prompt('Am I married? yes or no.');
+
+while(answerTwo.toLowerCase() !== 'y' && answerTwo.toLowerCase() !== 'n' && answerTwo.toLowerCase() !== 'yes' && answerTwo.toLowerCase() !== 'no') {
+  answerTwo = prompt('please answer yes or no.');
+}
+
+console.log('answerTwo: ', answerTwo);
+
+if (answerOne.toLowerCase() === 'n' || answerOne.toLowerCase() === 'no'){
+  alert('Correct! I can\'t wait to be, but currently, I am only engaged. The wedding is in 2021!');
+} else {
+  alert('Incorrect! I am happily engaged to my dear David, but we won\'t be married for another year and a half.');
+}
 
 // QUESTION 3
 
+
 // QUESTION 4
 
+
 // QUESTION 5
+
+
